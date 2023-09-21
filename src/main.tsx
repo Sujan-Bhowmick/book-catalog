14beaf5ae1663c47a1e11b8088c3a52b8b3c8cf8
Your Name <imsujan93@gmail.com>
2023-09-21 10:09:26 +0600

    port changed

--- src/main.tsx
+++ src/main.tsx
@@ -6,22 +6,10 @@ import {RouterProvider} from "react-router-dom"
 import { Provider } from 'react-redux'
 import { store } from './redux/store'
 
-// import { QueryClient, QueryClientProvider } from 'react-query';
-
-// const queryClient = new QueryClient({
-//   defaultOptions: {
-//     queries: {
-//       // Configure query options here if needed
-//     },
-//   },
-// });
-
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-  {/* <QueryClientProvider client={queryClient}> */}
     <Provider store={store}>
     <RouterProvider router= {routes} />
     </Provider>
-    {/* </QueryClientProvider> */}
   </React.StrictMode>,
 )
